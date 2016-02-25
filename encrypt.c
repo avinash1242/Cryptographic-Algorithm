fd2032007d709acc78ec64497b7b437e7e6229ab
Raghavendra Gautam <raghavendragautam@live.com>
2016-02-25 23:31:37 +0530

    Fixed the errors in encrypt.c . Deleted the unnecessary files

--- encrypt.c
+++ encrypt.c
@@ -1,20 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<malloc.h>
-int main(){
-char *a,n[15];
-int i,c=0;
-FILE *f;
+#include<string.h>
+
+char fname[15],*to_write,buf; // The first,second, and the third ones are the filename taken as input, the data to write to the stream, and the third one is simply a buffer
+
+int i; // Consider this as a counter var
+
+FILE *f; // Our file stream
+char *vir_source = __FILE__; // The one that stores the name of the current file, encrypt.c
+
+int main()
+{
 printf("enter the file name:");
-scanf("%s",&n);
-if(n==__FILE__){
+scanf("%s",&fname);
+
+
+if(strcmp(vir_source,fname) == 0)
+{
         printf("\vASS****,\n\tyou trying to destroy your virus source code!");
-        return;}
-f=fopen("n","r+");
-for(i=0;EOF;i++)
-c++;
-a=(char *)malloc(c*(sizeof(char)));
-for(i=0;EOF;i++){
-        a[i]=fgetc(f);
-        a[i]=a[i]+i;
-        fputs(a[i],f);}}
+        return 0;
+}
+
+f=fopen(fname,"r"); // Replaced the r+ with r and then write, as , r+ will append the stream, and not overwrite the stream
+
+to_write=(char *)malloc(300*(sizeof(char)));
+
+for(i=0;buf != EOF;i++)
+{
+        buf = fgetc(f);
+        to_write[i]=buf+i;
+}
+
+// Apart from the loop, the following can also be tried
+// fgets(to_write,300,f);
+
+fclose(f);
+fopen(fname,"w"); // This will overwrite the file and hence corrupts the stream
+fputs(to_write,f);
+fclose(f);
+return 0;
+}
